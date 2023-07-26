8a7e7773a19a30f23351b52b72e18f5d23728df3
Apach <pavel_andreev@inbox.ru>
2023-07-26 18:33:28 +0400

    Bonusmobaile

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int award = 100;
+        int replenishment = 1300;
+        int bonus;
+
+        if (replenishment >= 1000){
+            bonus = replenishment/ award;
+        } else {
+            bonus = 0;
+        }
+
+        int finalbalance = balance + replenishment + bonus;
+
+        System.out.println("Бонус " + bonus + " руб.");
+        System.out.println("Баланс " + finalbalance + " руб.");
+    }
+}
